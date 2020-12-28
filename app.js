96f4876c56b2ce731e621f15252646bddced4e95
lmcuongdev <cuong2000hy@gmail.com>
2020-12-29 01:54:06 +0700

    cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 
 const apiRouter = require("./routes");
 const HttpError = require("./models/http-error");
+const cors = require("cors");
 
 const app = express();
 
@@ -16,45 +17,36 @@ app.use(bodyParser.json());
 app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 // cors
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-  next();
-});
+app.use(cors());
 
 app.use("/api", apiRouter);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
-  throw error;
+	const error = new HttpError("Could not find this route.", 404);
+	throw error;
 });
 
 app.use((error, req, res, next) => {
-  if (req.file) {
-    fs.unlink(req.file.path, (err) => {
-      console.log(err);
-    });
-  }
-  if (res.headerSent) {
-    return next(error);
-  }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+	if (req.file) {
+		fs.unlink(req.file.path, (err) => {
+			console.log(err);
+		});
+	}
+	if (res.headerSent) {
+		return next(error);
+	}
+	res.status(error.code || 500);
+	res.json({ message: error.message || "An unknown error occurred!" });
 });
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8snuq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  )
-  .then(() => {
-    app.listen(process.env.PORT || 5000);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+	.connect(
+		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8snuq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+		{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+	)
+	.then(() => {
+		app.listen(process.env.PORT || 5000);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
